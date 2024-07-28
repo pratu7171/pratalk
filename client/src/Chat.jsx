9c2a1fb007c5e9973f2c71a7e70c5ac83cad8588
pratikg7171 <godhadepratik@gmail.com>
2024-07-29 04:04:10 +0530

    wss

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -23,7 +23,7 @@ export default function Chat() {
   }, [selectedUserId]);
   function connectToWs() {
     // const ws = new WebSocket('ws://localhost:4040');
-    const ws = new WebSocket('ws://pratalk-wjqr.onrender.com');
+    const ws = new WebSocket('wss://pratalk-wjqr.onrender.com');
 
     setWs(ws);
     ws.addEventListener('message', handleMessage);
