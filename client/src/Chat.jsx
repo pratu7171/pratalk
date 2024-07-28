4b399a35914319efc12d87aace39f8a3ae6ffa93
pratikg7171 <godhadepratik@gmail.com>
2024-07-29 03:51:29 +0530

    dotenv

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import Contact from "./Contact";
 const dotenv = require('dotenv');
 
+dotenv.config();
 
 export default function Chat() {
   const [ws,setWs] = useState(null);
